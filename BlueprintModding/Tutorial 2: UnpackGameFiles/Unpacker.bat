a220ac534c4552773d4ae5d9ff850ee29fa25278
Galactic Ale Project <69478954+GalacticAleProject@users.noreply.github.com>
2020-08-12 16:39:32 -0700

    Add files via upload

---
+++ BlueprintModding/Tutorial 2: UnpackGameFiles/Unpacker.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Epic Games\UE_4.22\Engine\Binaries\Win64\UnrealPak.exe" "C:\Users\YOUR_NAME\Desktop\Unpack\FSD-WindowsNoEditor.pak" -Extract C:\Users\YOUR_NAME\Desktop\Unpack\
+pause
